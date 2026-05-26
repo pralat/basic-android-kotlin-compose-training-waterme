9ed509850f52846b704922bbbb8e360c9f87374a
Mark Pralat <mpralat@gmail.com>
2026-05-26 12:55:48 -0400

    Forked codelab. Did the AGP upgrade and gradle toolchain updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -55,9 +56,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
