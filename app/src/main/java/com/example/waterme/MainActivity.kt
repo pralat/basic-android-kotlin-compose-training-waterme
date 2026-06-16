1073ab2de0159c87c4d2ee29cde717e2d6aba120
Mark Pralat <mpralat@gmail.com>
2026-06-16 12:19:50 -0400

    Call enableEdgeToEdge() to properly setup the status and navigation bars, and remove deprecated and unnecessary code in Theme.kt.

--- app/src/main/java/com/example/waterme/MainActivity.kt
+++ app/src/main/java/com/example/waterme/MainActivity.kt
@@ -19,6 +19,7 @@ package com.example.waterme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -28,6 +29,7 @@ import com.example.waterme.ui.theme.WaterMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             WaterMeTheme {
