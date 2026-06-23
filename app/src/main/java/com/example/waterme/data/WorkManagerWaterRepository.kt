8682f9d282575f12540e97026dd0091b7a8e17c7
Mark Pralat <mpralat@gmail.com>
2026-06-23 12:17:26 -0400

    Add scheduleReminder code to create workmanager.

--- app/src/main/java/com/example/waterme/data/WorkManagerWaterRepository.kt
+++ app/src/main/java/com/example/waterme/data/WorkManagerWaterRepository.kt
@@ -31,5 +31,18 @@ class WorkManagerWaterRepository(context: Context) : WaterRepository {
     override val plants: List<Plant>
         get() = DataSource.plants
 
-    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {}
+    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {
+        val data = Data.Builder()
+        data.putString(WaterReminderWorker.nameKey, plantName)
+
+        val reminderWorker = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(data.build())
+            .build()
+        workManager.enqueueUniqueWork(
+            plantName + duration,
+            ExistingWorkPolicy.REPLACE,
+            reminderWorker
+        )
+    }
 }
